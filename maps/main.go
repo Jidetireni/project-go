ec448956b85c06eaadde74a28dd9170667f33471
jidetireni <jtirenipraise@gmail.com>
2024-09-04 19:57:00 +0100

    learning maps

--- maps/main.go
+++ maps/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"maps"
+)
 
 func main() {
 	m := make(map[string]int)
@@ -9,4 +12,29 @@ func main() {
 	m["k2"] = 13
 
 	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println(v1)
+
+	v3 := m["k3"]
+	fmt.Println(v3)
+
+	fmt.Println("len:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	clear(m)
+	fmt.Println("map:", m)
+
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+
+	n2 := map[string]int{"foo": 1, "bar": 2}
+	if maps.Equal(n, n2) {
+		fmt.Println("n == n2")
+	}
 }
