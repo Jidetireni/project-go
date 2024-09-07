9acfee48f7a06130babeda0aa06c130dead75190
jidetireni <jtirenipraise@gmail.com>
2024-09-07 21:08:32 +0100

    chapter one

---
+++ chapters/one/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// func print_number_version_onr() {
+// 	number := 2
+
+// 	for number <= 100 {
+// 		if number%2 == 0 {
+// 			fmt.Println(number)
+// 		}
+// 		number += 1
+// 	}
+// }
+
+// func print_number_version_two() {
+// 	number := 2
+
+// 	for number <= 100 {
+// 		fmt.Println(number)
+// 	}
+// 	number += 2
+// }
+
+array := []string{ "apples", "bananas", "cucumbers", "dates", "elderberries" }
