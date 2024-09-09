014c9123f0c6edbb6fdae37a182cc9d2c7692141
jidetireni <jtirenipraise@gmail.com>
2024-09-09 21:16:21 +0100

    big O notatuion

---
+++ chapters/three/main.go
@@ -0,0 +1,24 @@
+package main
+
+func is_prime(number int) bool {
+	if number < 2 {
+		return false
+	}
+	for i := 2; i < number; i++ {
+		if number%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func isLeapYear(year int) bool {
+	if year%100 == 0 {
+		return true
+	} else if year%400 == 0 {
+		return true
+	} else if year%4 == 0 {
+		return true
+	}
+	return false
+}
